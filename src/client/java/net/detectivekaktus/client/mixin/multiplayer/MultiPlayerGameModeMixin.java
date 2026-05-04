adb2dc3b149fb98bf02a6e09d7c2f455bd1ef026
Artiom Astashonak <artiomastashonak@gmail.com>
2026-05-04 19:08:04 +0200

    mixin: prevent block destroying when disarmed or stunned

--- src/client/java/net/detectivekaktus/client/mixin/multiplayer/MultiPlayerGameModeMixin.java
+++ src/client/java/net/detectivekaktus/client/mixin/multiplayer/MultiPlayerGameModeMixin.java
@@ -1,21 +1,49 @@
 package net.detectivekaktus.client.mixin.multiplayer;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.MultiPlayerGameMode;
 import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
 
 import org.apache.commons.lang3.mutable.MutableObject;
 
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 
 import net.detectivekaktus.client.core.UiSoundManager;
+import net.detectivekaktus.effect.DotcEffects;
 
 @Mixin(MultiPlayerGameMode.class)
 public class MultiPlayerGameModeMixin {
+    @Shadow
+    @Final
+    private Minecraft minecraft;
+
+    @Inject(
+            method = "startDestroyBlock",
+            at = @At(value = "HEAD"),
+            cancellable = true
+    )
+    private void cancelDestroyBlock(BlockPos blockPos, Direction direction, CallbackInfoReturnable<Boolean> callbackInfo) {
+        var player = minecraft.player;
+        if (player == null)
+            return;
+
+        if (!player.hasEffect(DotcEffects.STUN) && !player.hasEffect(DotcEffects.DISARM))
+            return;
+
+        callbackInfo.setReturnValue(false);
+    }
+
     @ModifyExpressionValue(
             // After some WILD research the `MultiPlayerGameMode.useItem()` method
             // uses a lambda under the hood and its name in bytecode is the
