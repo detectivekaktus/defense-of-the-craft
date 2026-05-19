72c256511d5e3b063116a76985ad375b0aecdcd7
Artiom Astashonak <artiomastashonak@gmail.com>
2026-05-19 21:01:35 +0200

    refactor: move circle drawing logic from `AeonDiskAnimation` to `ParticleAnimation`

--- src/main/java/net/detectivekaktus/core/animation/AeonDiskAnimation.java
+++ src/main/java/net/detectivekaktus/core/animation/AeonDiskAnimation.java
@@ -11,47 +11,28 @@ public class AeonDiskAnimation extends ParticleAnimation {
     @Override
     public void playAnimation() {
         var radius = 1.5;
-
-        drawCircle(radius, ((cos, sin) -> {
-            level.sendParticles(
-                    particle,
-                    x + cos, y, z + sin,
-                    3,
-                    0, 0, 0,
-                    0
-            );
-        }));
-        drawCircle(radius, ((cos, sin) -> {
-            level.sendParticles(
-                    particle,
-                    x + cos, y + sin, z,
-                    3,
-                    0, 0, 0,
-                    0
-            );
-        }));
-        drawCircle(radius, ((cos, sin) -> {
-            level.sendParticles(
-                    particle,
-                    x, y + cos, z + sin,
-                    3,
-                    0, 0, 0,
-                    0
-            );
-        }));
-    }
-
-    private void drawCircle(double radius, TrigConsumer consumer) {
         var step = Math.PI / 15;
-        for (double angle = 0; angle < Math.PI * 2; angle += step) {
-            var cos = radius * Math.cos(angle);
-            var sin = radius * Math.sin(angle);
-            consumer.accept(cos ,sin);
-        }
-    }
 
-    @FunctionalInterface
-    interface TrigConsumer {
-        void accept(double cos, double sin);
+        drawCircle(radius, step, ((cos, sin) -> level.sendParticles(
+                particle,
+                x + cos, y, z + sin,
+                3,
+                0, 0, 0,
+                0
+        )));
+        drawCircle(radius, step, ((cos, sin) -> level.sendParticles(
+                particle,
+                x + cos, y + sin, z,
+                3,
+                0, 0, 0,
+                0
+        )));
+        drawCircle(radius, step, ((cos, sin) -> level.sendParticles(
+                particle,
+                x, y + cos, z + sin,
+                3,
+                0, 0, 0,
+                0
+        )));
     }
 }
