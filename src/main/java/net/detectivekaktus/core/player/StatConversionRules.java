b1bf9d138921d29fee01757400d17898b23380e3
Artiom Astashonak <artiomastashonak@gmail.com>
2026-06-05 10:37:23 +0200

    core: halve the attack speed bonus per agility

--- src/main/java/net/detectivekaktus/core/player/StatConversionRules.java
+++ src/main/java/net/detectivekaktus/core/player/StatConversionRules.java
@@ -5,7 +5,7 @@ public class StatConversionRules {
     public static final float HP_REGEN_PER_STRENGTH = 0.025f;
 
     public static final float ARMOR_PER_AGILITY = 0.1f;
-    public static final float ATTACK_SPEED_PER_AGILITY = 0.033f;
+    public static final float ATTACK_SPEED_PER_AGILITY = 0.015f;
 
     public static final float MANA_PER_INTELLIGENCE = 1.0f;
     public static final float MANA_REGEN_PER_INTELLIGENCE = 0.2f;
