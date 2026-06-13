08e7f6a47ac382aa7a5d84f538a5a1528c713682
Artiom Astashonak <artiomastashonak@gmail.com>
2026-06-13 17:31:17 +0200

    fix: make `DotcMusicSounds` fields final

--- src/main/java/net/detectivekaktus/sound/music/DotcMusicSounds.java
+++ src/main/java/net/detectivekaktus/sound/music/DotcMusicSounds.java
@@ -10,13 +10,13 @@ import net.detectivekaktus.DefenseOfTheCraft;
 import net.detectivekaktus.sound.DotcSounds;
 
 public class DotcMusicSounds {
-    public static SoundEvent RADIANT_THEME = DotcSounds.register("music_radiant_theme");
-    public static ResourceKey<JukeboxSong> RADIANT_THEME_KEY = ResourceKey.create(
+    public static final SoundEvent RADIANT_THEME = DotcSounds.register("music_radiant_theme");
+    public static final ResourceKey<JukeboxSong> RADIANT_THEME_KEY = ResourceKey.create(
             Registries.JUKEBOX_SONG, ResourceLocation.fromNamespaceAndPath(DefenseOfTheCraft.MOD_ID, "music_radiant_theme")
     );
 
-    public static SoundEvent DIRE_THEME = DotcSounds.register("music_dire_theme");
-    public static ResourceKey<JukeboxSong> DIRE_THEME_KEY = ResourceKey.create(
+    public static final SoundEvent DIRE_THEME = DotcSounds.register("music_dire_theme");
+    public static final ResourceKey<JukeboxSong> DIRE_THEME_KEY = ResourceKey.create(
             Registries.JUKEBOX_SONG, ResourceLocation.fromNamespaceAndPath(DefenseOfTheCraft.MOD_ID, "music_dire_theme")
     );
 
