ce9d58661d1e8a5a615555a823e674341c5a69e1
Artiom Astashonak <artiomastashonak@gmail.com>
2026-06-13 17:09:33 +0200

    item: add radiant theme music disc

---
+++ src/main/java/net/detectivekaktus/sound/music/DotcMusicSounds.java
@@ -0,0 +1,19 @@
+package net.detectivekaktus.sound.music;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.item.JukeboxSong;
+
+import net.detectivekaktus.DefenseOfTheCraft;
+import net.detectivekaktus.sound.DotcSounds;
+
+public class DotcMusicSounds {
+    public static SoundEvent RADIANT_THEME = DotcSounds.register("music_radiant_theme");
+    public static ResourceKey<JukeboxSong> RADIANT_THEME_KEY = ResourceKey.create(
+            Registries.JUKEBOX_SONG, ResourceLocation.fromNamespaceAndPath(DefenseOfTheCraft.MOD_ID, "music_radiant_theme")
+    );
+
+    public static void initialize() { }
+}
